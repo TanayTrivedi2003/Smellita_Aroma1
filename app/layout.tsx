4be0182ac778f8d9574ebf0830ff1936045c3448
Tanay Trivedi <triveditanay20@gmail.com>
2026-02-20 16:14:49 +0530

    fix icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,15 +25,15 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/logo-amit.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/logo-amit.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/images/logo-amit.png",
         type: "image/svg+xml",
       },
     ],
